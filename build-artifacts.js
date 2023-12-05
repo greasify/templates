b27e536bfc93d7ff81a7872dc19d9585c44682bf
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-12-05 20:01:51 +0800

    initial commit

---
+++ build-artifacts.js
@@ -0,0 +1,49 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import Zip from "adm-zip";
+
+/** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+export default async function build({ github, context }) {
+  const artifactsDirectoryPath = path.join(process.cwd(), "artifacts");
+  const templatesDirectoryPath = path.join(process.cwd(), "templates");
+  const templatesPath = await fs.readdir(templatesDirectoryPath);
+
+  try {
+    await fs.mkdir(artifactsDirectoryPath);
+  } catch {}
+
+  const release = await github.rest.repos.createRelease({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    draft: false,
+    prerelease: false,
+    tag_name: 'latest'
+  });
+
+  const uploadArtifact = async (name, path) => {
+    await github.rest.repos.uploadReleaseAsset({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      origin: release.data.upload_url,
+      release_id: release.data.id,
+      name,
+      data: await fs.readFile(path),
+    });
+  }
+
+  async function createZipArchive(name) {
+    const zip = new Zip();
+    const fileName = `${name}.zip`
+    const outputFile = path.resolve(artifactsDirectoryPath, fileName);
+    zip.addLocalFolder(path.resolve(templatesDirectoryPath, name));
+    zip.writeZip(outputFile);
+    await uploadArtifact(fileName, outputFile);
+    console.log(`Created "${fileName}" successfully`);
+  }
+
+  for (const templateName of templatesPath) {
+    await createZipArchive(templateName);
+  }
+
+  console.log('Build artifacts done')
+}
---
+++ templates/ts/README.md
@@ -0,0 +1,26 @@
+# Greasify Template
+
+## Usage
+
+Install dependencies
+
+```bash
+npm install
+```
+
+Run development server
+
+```bash
+npm run dev
+```
+
+Build for production
+
+```bash
+npm run build
+```
+
+## Tampermonkey Setup
+
+- Allow Tampermonkey's access to [local file URI's](https://tampermonkey.net/faq.php?ext=dhdg#Q204)
+- Install proxy script from `dist/{{NAME}}.proxy.user.js` or `http://localhost:3000`
